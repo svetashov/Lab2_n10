76b2a46b103239ec1ad86d072f45996c4462ce71
svetashov <grandais1@gmail.com>
2020-05-30 15:29:22 +0300

    added search by date function in menu_utils

---
+++ Lab2_n10/menu_utils.cpp
@@ -0,0 +1,71 @@
+#include "menu_utils.h"
+#include "Utils.h"
+
+int menu_show()
+{
+	const int menu_size = 9;
+
+	std::cout << std::endl;
+	std::cout << "1: Посмотреть данные о багажах." << std::endl;
+	std::cout << "2: Добавить багаж." << std::endl;
+	std::cout << "3: Изменить багаж." << std::endl;
+	std::cout << "4: Удалить багаж." << std::endl;
+	std::cout << "5: Найти багажи." << std::endl;
+	std::cout << "6: Очистить контейнер." << std::endl;
+	std::cout << "7: Добавить данные из файла." << std::endl;
+	std::cout << "8: Сохранить данные в файл." << std::endl;
+	std::cout << "9: Найти общий вес багажей." << std::endl;
+	std::cout << "0: Exit" << std::endl;
+	const int choice = get_number(0, menu_size, "");
+	std::cout << std::endl;
+	return choice;
+}
+
+int menu_search_show()
+{
+	const int menu_size = 4;
+
+	std::cout << std::endl;
+	std::cout << "1: Найти по номеру рейса." << std::endl;
+	std::cout << "2: Найти по дате вылета." << std::endl;
+	std::cout << "3: Найти по пункту назначения." << std::endl;
+	std::cout << "4: Найти по весу багажа." << std::endl;
+	std::cout << "0: Exit" << std::endl;
+	const int choice = get_number(0, menu_size, "");
+	std::cout << std::endl;
+	return choice;
+}
+
+int menu_select_algorithm()
+{
+	const int menu_size = 2;
+
+	std::cout << std::endl;
+	std::cout << "1: Бинарный поиск." << std::endl;
+	std::cout << "2: Линейный поиск." << std::endl;
+	const int choice = get_number(1, menu_size, "");
+	std::cout << std::endl;
+	return choice;
+}
+
+baggage_stock search_surname(const baggage_stock& stock, bool use_binary_search = false)
+{
+	const std::string surname = get_string(1, "Введите фамилию владельца багажа");
+	if (use_binary_search)
+		return stock.binary_search<std::string>(surname, [](const Baggage& b) { return b.surname; });
+	
+	return stock.linear_search([&surname](const Baggage& baggage) { return baggage.surname == surname; });
+}
+
+baggage_stock search_date(const baggage_stock& stock, bool use_binary_search = false)
+{
+	Date date = Date();
+	std::cout << "Введите дату: " << std::endl;
+	read(date);
+	if (use_binary_search)
+		return stock.binary_search<Date>(date, [](const Baggage& b) { return b.dep_date; });
+
+	return stock.linear_search([&date](const Baggage& baggage) { return baggage.dep_date == date; });
+}
+
+
---
+++ Lab2_n10/menu_utils.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "baggage_stock.h"
+
+int menu_show();
+
+int menu_search_show();
+
+void menu();
+
+void menu_search(const baggage_stock& stock);
+
+void menu_print(const baggage_stock& stock);
+
+void menu_add(baggage_stock& stock);
+
+void menu_update(baggage_stock& stock);
+
+void menu_remove(baggage_stock& stock);
+
+void menu_clear(baggage_stock& stock);
+
+void menu_add_from_file(baggage_stock& stock);
+
+void menu_save_to_file(baggage_stock& stock);
